d6e51d4122da3e9510de84e4a6e35d36a8b9d126
Mahdi <amiralmasi021@gmail.com>
2024-10-21 16:09:43 +0330

    Debug mapping to json file

--- src/main/java/com/multiThread/MultiThread/entity/Error.java
+++ src/main/java/com/multiThread/MultiThread/entity/Error.java
@@ -1,14 +1,14 @@
 package com.multiThread.MultiThread.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Error implements Serializable {
     private String FILE_NAME;
     private Long RECORD_NUMBER;
--- src/main/java/com/multiThread/MultiThread/service/ErrorService.java
+++ src/main/java/com/multiThread/MultiThread/service/ErrorService.java
@@ -1,6 +1,8 @@
 package com.multiThread.MultiThread.service;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.multiThread.MultiThread.entity.Error;
@@ -14,8 +16,10 @@ import java.util.List;
 
 @Service
 public class ErrorService {
+
     @Value("${errors.file.json.fileName}")
     private String filePath;
+
     public void writeErrors(List<Error> errorList) {
         ObjectMapper objectMapper = new ObjectMapper();
         // pretty print
@@ -23,16 +27,20 @@ public class ErrorService {
 
         File file = new File(filePath);
         List<Error> existingErrors = new ArrayList<>();
+
         try {
-            // read existing errors from the json file if it exists
-            if (file.exists()) {
+            // read existing errors from the json file if it exists and is not empty
+            if (file.exists() && file.length() != 0) {
                 existingErrors = objectMapper.readValue(file, new TypeReference<List<Error>>() {});
             }
             // add new errors to the existing list
             existingErrors.addAll(errorList);
 
+            // write the updated error list to the file
             objectMapper.writeValue(file, existingErrors);
             System.out.println("Errors have been updated in the json file.");
+        } catch (JsonParseException | JsonMappingException e) {
+            System.err.println("Error parsing the JSON file: " + e.getMessage());
         } catch (IOException e) {
             e.printStackTrace();
         }
